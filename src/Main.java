d84cabcac13dfc8d473903a94f1111c0f8cd6e3c
Vlad340 <abcd-mail@yandex.ru>
2019-09-22 22:50:53 +0300

    changed the variable name with 'Purchases'

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,25 @@ public class Main {
 /**
  * Calculation of bonuses on the purchase amount and the total amount of all previous purchases
  */
-        long amountAllPurchasess = 145_000_00L;
-        long amountCurrentPurchasess = 15_000_00L;
+        long amountAllPurchases = 145_000_00L;
+        long amountCurrentPurchases = 15_000_00L;
         long amountDiscountGoods = 1_500_00L;
         long nominalGiftCardPay = 500_00L;
-        long nominalGiftCardPurchasess = 300_00L;
+        long nominalGiftCardPurchases = 300_00L;
         long nominalBonusesNumberGoodSpecialOffer = 500_00L;
-        long nominalBonusesAmountSpecialOffer = 200_00L;
         long amountGoodsSpecialOffer = 5_000_00L;
         int numberGoodsSpecialOffer = 2;
         long amountSpecialOffer = 400_00L;
+        long nominalBonusesAmountSpecialOffer = 200_00L;
         long amountPayBonuses = 200_00L;
 
-        amountAllPurchasess += amountCurrentPurchasess;
-        long amountAccuredBonuses = amountCurrentPurchasess - amountDiscountGoods - nominalGiftCardPurchasess - nominalGiftCardPay - amountSpecialOffer - amountPayBonuses - amountGoodsSpecialOffer;
+        amountAllPurchases += amountCurrentPurchases;
+        long amountAccuredBonuses = amountCurrentPurchases - amountDiscountGoods - nominalGiftCardPurchases - nominalGiftCardPay - amountSpecialOffer - amountPayBonuses - amountGoodsSpecialOffer;
         long nominalBonuses = 50_00L;
-        if (amountAllPurchasess > 15_001_00) {
+        if (amountAllPurchases > 15_001_00) {
             nominalBonuses = 70_00L;
             }
-        if (amountAllPurchasess > 150_000_00) {
+        if (amountAllPurchases > 150_000_00) {
             nominalBonuses = 100_00L;
         }
         long bonuses = (amountAccuredBonuses / 1_000_00)*nominalBonuses;

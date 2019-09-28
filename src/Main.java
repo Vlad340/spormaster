a1b1d3eb945d48ba16ca7e978b998ba9ebfffcee
Vlad340 <abcd-mail@yandex.ru>
2019-09-28 15:56:08 +0300

    Logical blocks are separated by blank lines, numbers moved to variables, code aligned, comments deleted

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-/**
- * Calculation of bonuses on the purchase amount and the total amount of all previous purchases
- */
+
         long amountAllPurchases = 145_000_00L;
         long amountCurrentPurchases = 15_000_00L;
         long amountDiscountGoods = 1_500_00L;
@@ -13,29 +11,44 @@ public class Main {
         int numberGoodsSpecialOffer = 2;
         long amountSpecialOffer = 400_00L;
         long nominalBonusesAmountSpecialOffer = 200_00L;
+        long minAmountSpecialOffer=1_000_00L;
         long amountPayBonuses = 200_00L;
+        long nominalBonusesBlueCard = 50_00L;
+        long nominalBonusesSilverCard = 70_00L;
+        long nominalBonusesGoldCard = 100_00L;
+        long minAmountAllPurchasesSilverCard=15_001_00L;
+        long minAmountAllPurchasesGoldCard=150_000_00L;
+        long bonusPurchaseRate=1_000_00L;
+        int numberOfUnitsInOneBonus=100;
 
         amountAllPurchases += amountCurrentPurchases;
         long amountAccuredBonuses = amountCurrentPurchases - amountDiscountGoods - nominalGiftCardPurchases - nominalGiftCardPay - amountSpecialOffer - amountPayBonuses - amountGoodsSpecialOffer;
-        long nominalBonuses = 50_00L;
-        if (amountAllPurchases > 15_001_00) {
-            nominalBonuses = 70_00L;
-            }
-        if (amountAllPurchases > 150_000_00) {
-            nominalBonuses = 100_00L;
+
+        long nominalBonuses=nominalBonusesBlueCard;
+
+        if (amountAllPurchases > minAmountAllPurchasesSilverCard) {
+            nominalBonuses = nominalBonusesSilverCard;
         }
-        long bonuses = (amountAccuredBonuses / 1_000_00)*nominalBonuses;
+
+        if (amountAllPurchases > minAmountAllPurchasesGoldCard) {
+            nominalBonuses = nominalBonusesGoldCard;
+        }
+
+        long bonuses = (amountAccuredBonuses / bonusPurchaseRate)*nominalBonuses;
         bonuses += (numberGoodsSpecialOffer * nominalBonusesNumberGoodSpecialOffer) ;
-        if (amountSpecialOffer > 1_000_00) {
+
+        if (amountSpecialOffer > minAmountSpecialOffer) {
             bonuses += nominalBonusesAmountSpecialOffer;
         }
-        if (nominalBonuses == 50_00) {
+
+        if (nominalBonuses == nominalBonusesBlueCard) {
             System.out.println("Синяя карта");
-        } else if (nominalBonuses == 70_00) {
+        } else if (nominalBonuses == nominalBonusesSilverCard) {
             System.out.println("Серебряная карта");
         } else {
             System.out.println("Золотая карта");
         }
-        System.out.println("Начислено:"+bonuses/100+" бонусов");
+
+        System.out.println("Начислено:"+bonuses/numberOfUnitsInOneBonus+" бонусов");
     }
 }
